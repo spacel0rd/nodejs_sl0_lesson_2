356e6fe1d579df47754716221cb91b60d0eeab15
spacel0rd <sl0spb@gmail.com>
2016-08-31 17:20:24 +0300

    Create package.json

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 /**
  * Created by sl0 on 31.08.2016.
  */
-ss
+
+var read = require('read');
+
+read({ prompt : 'Введите число (1 или 2) ' }, function (err, user) {
+		console.log(user);
+		process.stdin.destroy();
+})
+ 
