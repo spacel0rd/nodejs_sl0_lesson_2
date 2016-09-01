e8a38459ce674e21cc970b6aee7565713943121c
spacel0rd <sl0spb@gmail.com>
2016-09-01 13:59:03 +0300

    Певый вариант

--- index.js
+++ index.js
@@ -2,9 +2,48 @@
  * Created by sl0 on 31.08.2016.
  */
 
-var read = require('read');
+var read = require('readline');
 
-read({ prompt : 'Введите число (1 или 2) ' }, function (err, user) {
-		console.log(user);
-		process.stdin.destroy();
+
+function rand_num() {
+	return Math.floor(Math.random() * 2) + 1;
+}
+
+var rl = read.createInterface({
+	input: process.stdin,
+	output: process.stdout
 });
+
+rl.write('Введите число (1 или 2) или 3 для выхода\n');
+
+rl.on('line', function(cmd) {
+	var answer = rand_num();
+	if (cmd == answer) {
+		console.log('Congratulation! You win.');
+	}
+	else if (cmd == 3) {
+		console.log('It is the end.');
+		this.close(); // закрываем обработчик
+	}
+	else if (cmd > 2 || cmd < 1 ){
+		console.log('Только 1 или 2');
+	}
+	else {console.log('Sorry. You lose.');}
+	
+});
+
+/*
+
+		var answer = rand_num();
+		if (user_num == answer) {
+			console.log('Congratulation! You win.');
+		}
+		else if (user_num == 3) {
+			console.log('dd');
+		}
+		else {
+			console.log('Sorry. You lose.');
+		}
+*/
+
+
